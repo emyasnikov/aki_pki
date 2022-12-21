e402891ac2fd00561c09d67e2389779220c16e07
Technotise <Jonnymo9@outlook.de>
2022-12-21 02:10:24 +0100

    Hinzufügen von Formerkennung als ersten Schritt

--- haarcascade.py
+++ haarcascade.py
@@ -1,47 +1,65 @@
+from turtle import circle
 import cv2
+import numpy as np
+
+def erkennung(wert):
+    for detector in detectors.values():
+        if detector["form"] == wert:
+            faces = detector["cascade"].detectMultiScale(image_gray)
+       
+            for x, y, width, height in faces:
+                cv2.rectangle(image, (x, y), (x + width, y + height), color = detector["color"], thickness = 2)
+                cv2.putText(image, detector["text"], (x, y - 5), font, 0.5, detector["color"], 2)
 
 detectors = {
     "vz123": {
         "code": "vz123",
         "color": (50, 200, 50),
         "name": "Arbeitsstelle",
-        "text": "Arbeitsstelle"
+        "text": "Arbeitsstelle",
+        "form": "Dreieck"
     },
     "vz205": {
         "code": "vz205",
         "color": (50, 50, 200),
         "name": "Vorfahrt gewähren!",
-        "text": "Vorfahrt gewaehren"
+        "text": "Vorfahrt gewaehren",
+        "form": "Dreieck"
     },
     "vz206": {
         "code": "vz206",
         "color": (50, 50, 200),
         "name": "Halt! Vorfahrt gewähren!",
-        "text": "Stopp"
+        "text": "Stopp",
+        "form": "Achteck"
     },
     "vz267": {
         "code": "vz267",
         "color": (200, 50, 150),
         "name": "Verbot der Einfahrt",
-        "text": "Verbot der Einfahrt"
+        "text": "Verbot der Einfahrt",
+        "form": "Kreis"
     },
     "vz306": {
         "code": "vz306",
         "color": (200, 50, 50),
         "name": "Vorfahrtstraße",
-        "text": "Vorfahrtstrasse"
+        "text": "Vorfahrtstrasse",
+        "form": "Viereck"
     },
     "vz350": {
         "code": "vz350",
         "color": (50, 150, 250),
         "name": "Fußgängerüberweg",
-        "text": "Fusgaengerueberweg"
+        "text": "Fusgaengerueberweg",
+        "form": "Viereck"
     }
 }
 
 capture = cv2.VideoCapture(0)
 font = cv2.FONT_HERSHEY_SIMPLEX
 
+#Einlesen XML
 for code, detector in detectors.items():
     detector["cascade"] = cv2.CascadeClassifier(f"detectors/{code}.detector.xml")
 
@@ -49,12 +67,39 @@ while True:
     _, image = capture.read()
     image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    for detector in detectors.values():
-        faces = detector["cascade"].detectMultiScale(image_gray)
+#Formen
+    #Linien
+    ret, thresh = cv2.threshold(image_gray, 127, 255, cv2.THRESH_BINARY)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    #Kreis
+    circles = cv2.HoughCircles(image_gray, cv2.HOUGH_GRADIENT, 1.2, 100)
 
-        for x, y, width, height in faces:
-            cv2.rectangle(image, (x, y), (x + width, y + height), color = detector["color"], thickness = 1)
-            cv2.putText(image, detector["text"], (x, y - 5), font, 0.5, detector["color"], 1)
+#Erkennung
+    for contour in contours:
+        approx = cv2.approxPolyDP(contour, 0.01*cv2.arcLength(contour,True), True)
+
+        #Dreieck
+        if len(approx) == 3:
+            erkennung("Dreieck")
+
+        #Quadrat & Rechteck
+        elif len(approx) == 4:
+            (x,y, w, h) = cv2.boundingRect(approx)
+            ar = w / float(h)
+            if ar >= 0.95 and ar <= 1.05:
+                erkennung("Viereck")
+            else:
+                erkennung("Rechteck")
+
+        #Achteck
+        elif len(approx) == 8:
+            erkennung("Achteck")
+
+    #Kreis
+    if circles is not None:
+        circles = np.round(circles[0, :]).astype("int")
+        for (x, y, r) in circles:
+            erkennung("Kreis")
 
     cv2.imshow("Camera", image)
 
