a8f6f70f4f21a8d27230443570abb0773767037d
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2022-12-21 08:28:46 +0100

    Refactored forms.

--- haarcascade.py
+++ haarcascade.py
@@ -6,7 +6,7 @@ def erkennung(wert):
     for detector in detectors.values():
         if detector["form"] == wert:
             faces = detector["cascade"].detectMultiScale(image_gray)
-       
+
             for x, y, width, height in faces:
                 cv2.rectangle(image, (x, y), (x + width, y + height), color = detector["color"], thickness = 2)
                 cv2.putText(image, detector["text"], (x, y - 5), font, 0.5, detector["color"], 2)
@@ -17,42 +17,42 @@ detectors = {
         "color": (50, 200, 50),
         "name": "Arbeitsstelle",
         "text": "Arbeitsstelle",
-        "form": "Dreieck"
+        "form": "triangle"
     },
     "vz205": {
         "code": "vz205",
         "color": (50, 50, 200),
         "name": "Vorfahrt gewähren!",
         "text": "Vorfahrt gewaehren",
-        "form": "Dreieck"
+        "form": "triangle"
     },
     "vz206": {
         "code": "vz206",
         "color": (50, 50, 200),
         "name": "Halt! Vorfahrt gewähren!",
         "text": "Stopp",
-        "form": "Achteck"
+        "form": "octagon"
     },
     "vz267": {
         "code": "vz267",
         "color": (200, 50, 150),
         "name": "Verbot der Einfahrt",
         "text": "Verbot der Einfahrt",
-        "form": "Kreis"
+        "form": "circle"
     },
     "vz306": {
         "code": "vz306",
         "color": (200, 50, 50),
         "name": "Vorfahrtstraße",
         "text": "Vorfahrtstrasse",
-        "form": "Viereck"
+        "form": "square"
     },
     "vz350": {
         "code": "vz350",
         "color": (50, 150, 250),
         "name": "Fußgängerüberweg",
         "text": "Fusgaengerueberweg",
-        "form": "Viereck"
+        "form": "square"
     }
 }
 
@@ -67,39 +67,39 @@ while True:
     _, image = capture.read()
     image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-#Formen
-    #Linien
+# Forms
+    # Lines
     ret, thresh = cv2.threshold(image_gray, 127, 255, cv2.THRESH_BINARY)
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    #Kreis
+    # Circle
     circles = cv2.HoughCircles(image_gray, cv2.HOUGH_GRADIENT, 1.2, 100)
 
 #Erkennung
     for contour in contours:
         approx = cv2.approxPolyDP(contour, 0.01*cv2.arcLength(contour,True), True)
 
-        #Dreieck
+        # Triangle
         if len(approx) == 3:
-            erkennung("Dreieck")
+            erkennung("triangle")
 
-        #Quadrat & Rechteck
+        # Square & rectangle
         elif len(approx) == 4:
             (x,y, w, h) = cv2.boundingRect(approx)
             ar = w / float(h)
             if ar >= 0.95 and ar <= 1.05:
-                erkennung("Viereck")
+                erkennung("square")
             else:
-                erkennung("Rechteck")
+                erkennung("rectangle")
 
-        #Achteck
+        # Octagon
         elif len(approx) == 8:
-            erkennung("Achteck")
+            erkennung("octagon")
 
-    #Kreis
+    # Circle
     if circles is not None:
         circles = np.round(circles[0, :]).astype("int")
         for (x, y, r) in circles:
-            erkennung("Kreis")
+            erkennung("circle")
 
     cv2.imshow("Camera", image)
 
